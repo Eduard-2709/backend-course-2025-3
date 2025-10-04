a7f62b53c207edafafdcb1dac58833a5750aa5da
Едуард bc2025-3 <eduardcer2709@gmail.com>
2025-10-04 14:11:18 +0300

    Виправити повідомлення про помилку через відсутність вхідного параметра

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const { program } = require('commander');
 const fs = require('fs');
 
 program
-  .requiredOption('-i, --input <path>', 'Шлях до вхідного JSON-файлу')
-  .option('-o, --output <path>', 'Шлях до вихідного файлу')
-  .option('-d, --display', 'Показати результати в консолі')
-  .option('-v, --variety', 'Показати різноманітність квітів')
-  .option('-l, --length <number>', 'Відображати лише записи з довжиною пелюсток, більшою за вказану', parseFloat);
+    .option('-i, --input <path>', 'Path to input JSON file')
+    .option('-o, --output <path>', 'Path to output file')
+    .option('-d, --display', 'Display results in console')
+    .option('-v, --variety', 'Display flower variety')
+    .option('-l, --length <number>', 'Display only records with petal length greater than specified', parseFloat);
 
 program.parse(process.argv);
 
@@ -14,23 +14,23 @@ const options = program.opts();
 
 // Перевірка обов'язкового параметра
 if (!options.input) {
-  console.error('Будь ласка, вкажіть вхідний файл');
-  process.exit(1);
+    console.error('Please, specify input file');
+    process.exit(1);
 }
 
 // Перевірка існування файлу
 if (!fs.existsSync(options.input)) {
-  console.error('Не вдається знайти вхідний файл');
-  process.exit(1);
+    console.error('Cannot find input file');
+    process.exit(1);
 }
 
 // Читання файлу
 let fileContent;
 try {
-  fileContent = fs.readFileSync(options.input, 'utf8');
+    fileContent = fs.readFileSync(options.input, 'utf8');
 } catch (error) {
-  console.error('Помилка читання файлу:', error.message);
-  process.exit(1);
+    console.error('Error reading file:', error.message);
+    process.exit(1);
 }
 
 // Парсинг JSON (кожен рядок - окремий JSON об'єкт)
@@ -40,18 +40,18 @@ const data = lines.map(line => JSON.parse(line));
 // Фільтрація даних за довжиною пелюстки
 let filteredData = data;
 if (options.length !== undefined) {
-  filteredData = data.filter(item => item['petal.length'] > options.length);
+    filteredData = data.filter(item => item['petal.length'] > options.length);
 }
 
 // Формування результату
 const results = filteredData.map(item => {
-  let output = `${item['petal.length']} ${item['petal.width']}`;
-  
-  if (options.variety) {
-    output += ` ${item.variety}`;
-  }
-  
-  return output;
+    let output = `${item['petal.length']} ${item['petal.width']}`;
+
+    if (options.variety) {
+        output += ` ${item.variety}`;
+    }
+
+    return output;
 });
 
 // Формування виводу
@@ -59,19 +59,19 @@ const outputText = results.join('\n');
 
 // Вивід результатів
 if (!options.output && !options.display) {
-  // Якщо не задано жодного з необов'язкових параметрів - нічого не виводимо
-  process.exit(0);
+    // Якщо не задано жодного з необов'язкових параметрів - нічого не виводимо
+    process.exit(0);
 }
 
 if (options.display) {
-  console.log(outputText);
+    console.log(outputText);
 }
 
 if (options.output) {
-  try {
-    fs.writeFileSync(options.output, outputText, 'utf8');
-  } catch (error) {
-    console.error('Помилка запису файлу:', error.message);
-    process.exit(1);
-  }
+    try {
+        fs.writeFileSync(options.output, outputText, 'utf8');
+    } catch (error) {
+        console.error('Error writing file:', error.message);
+        process.exit(1);
+    }
 }
